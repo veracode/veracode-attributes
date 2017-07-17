d265259cef242afef1c5051b355d465cf0d64bbe
U-VERACODE\blizano <blizano@blizano-800c.Veracode.local>
2017-07-17 16:48:59 -0400

    initial commit

---
+++ build.bat
@@ -0,0 +1,1 @@
+@C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /platform:anycpu /target:library /debug:full /pdb:VeracodeAttributes.pdb /out:VeracodeAttributes.dll Attributes.cs Properties\AssemblyInfo.cs  
